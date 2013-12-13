18ccbe918dc1921c961f0657b938f18ddcbafc54
zhyb <zhyb@zhyb.local>
2013-12-13 20:35:24 +0800

    Initial Commit

---
+++ BlocksDog/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  BlocksDog
+//
+//  Created by zhyb on 13-12-13.
+//  Copyright (c) 2013年 zhyb. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
